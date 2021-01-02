225e29e46e327d558b578266622baf9f203fdb98
hninthazinnwe <hninnwe2017htznwh@gmail.com>
2021-01-03 01:40:00 +0630

    initial

--- pos/src/main.js
+++ pos/src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router';
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+import {routes} from './routes';
+import Vuesax from 'vuesax';
+import vuesax from 'vuesax/dist/vuesax.css'; //   Vuesax styles
 
 Vue.config.productionTip = false
+Vue.use(VueRouter);
+Vue.use(VueAxios, axios);
+Vue.use(Vuesax, {vuesax});
+ 
+const router = new VueRouter({
+    mode: 'history',
+    routes: routes
+});
 
 new Vue({
+  router: router,
   render: h => h(App),
 }).$mount('#app')
